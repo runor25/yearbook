3c12d00f898a94619ad3aeeccf2e2515931c5a81
runor25 <oruno.awhie@gmail.com>
2024-08-06 09:09:58 +0100

    new

--- css/main.js
+++ css/main.js
@@ -1,6 +1,9 @@
 // const hoverElement = document.querySelector(".page-title");
 // const affectedElement = document.querySelector(".hrs");
 
+const passwordInput = document.querySelector(".passin");
+const showPasswordBtn = document.querySelector(".btn-show-pass");
+
 // hoverElement.addEventListener("mouseover", () => {
 //   affectedElement.style.backgroundColor = "red";
 //   affectedElement.style.width = "";
@@ -30,3 +33,38 @@ if (hoverElement) {
 } else {
   console.error("Element with class '.page-title' not found!");
 }
+
+// label fix
+
+const inputs = document.querySelectorAll(".input100");
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    input.classList.toggle("has-val", input.value.trim() !== "");
+    console.log("Input value:", input.value);
+    console.log("Class list:", input.classList);
+  });
+});
+
+const inputo = document.querySelectorAll(".inputpass");
+
+inputo.forEach((inpu) => {
+  if (inpu > "") {
+    inpu.classList.add("has-val");
+    inpu.classList.toggle("has-val", inpu.value.trim() !== "");
+    console.log("Input value:", inpu.value);
+    console.log("Class list:", inpu.classList);
+  }
+});
+
+//      show password
+
+showPasswordBtn.addEventListener("click", () => {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    showPasswordBtn.classList.add("active"); // Optional: Add a class to change icon
+  } else {
+    passwordInput.type = "password";
+    showPasswordBtn.classList.remove("active"); // Optional: Remove the class
+  }
+});
